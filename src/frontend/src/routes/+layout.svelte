9f9354683d328b3fa24fa93050414a877b385561
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-03 13:33:56 +0700

    add sidebar login signup

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
 	import '../app.css';
 	import Sidebar from '$lib/Sidebar.svelte';
+
+	const sidebarItems = [
+		{
+			text: 'Sign Up',
+			href: '/signup',
+			subitems: null
+		},
+		{
+			text: 'Log In',
+			href: null,
+			subitems: [
+				{ text: 'Member', href: '/login', subitems: null },
+				{ text: 'Admin', href: '/logout', subitems: null },
+				{ text: 'Developer', href: '/logout', subitems: null }
+			]
+		}
+	];
 </script>
 
 <div class="layout">
+	<Sidebar items={sidebarItems} />
 	<main>
 		<slot></slot>
 	</main>
 </div>
+
+<style>
+	.layout {
+		display: flex;
+		height: 100vh;
+	}
+	main {
+		flex: 1;
+		padding: 1rem;
+		background-color: #ecf0f1;
+	}
+</style>
