90ee7f0ce97e6a8a6a93541d205ec6736d3421d9
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-03 13:30:58 +0700

    feat sidebar member

---
+++ src/frontend/src/routes/member/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Sidebar from '$lib/Sidebar.svelte';
+
+	const sidebarItems = [
+		{
+			text: 'Activity',
+			href: null,
+			subitems: [
+				{ text: 'Your Activities', href: '/member/activities', subitems: null },
+				{ text: 'Your Proposals', href: '/member/activities/proposals', subitems: null }
+			]
+		},
+		{
+			text: 'Propose New Activity',
+			href: '/member/activities/propose',
+			subitems: null
+		}
+	];
+</script>
+
+<div class="layout">
+	<Sidebar items={sidebarItems} />
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+	.layout {
+		display: flex;
+		height: 100vh;
+	}
+	main {
+		flex: 1;
+		padding: 1rem;
+		background-color: #ecf0f1;
+	}
+</style>
---
+++ src/frontend/src/routes/member/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Test Member</h1>
