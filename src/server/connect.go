1704b58b7a8d61b12d8ce9d07d341ad072bd8138
Natchapol Lebkrut <natchaponlt@gmail.com>
2024-11-03 14:35:31 +0700

    change func main() to connect()

--- src/server/connect.go
+++ src/server/connect.go
@@ -1,30 +1,29 @@
 package main
+
 //import essentials libraries
 import (
-	"fmt"
 	"database/sql"
-	"net/http"
-	"html/template"
 	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-}
+)
 
 var (
 	db *sql.DB
 )
 
-func main() {
+func connect() {
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/activitymanagement")
 	if err != nil {
 		panic(err.Error())
 	}
 	// the upper code is to connect to the database and error handling
 	// Ensure the connection is valid
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	defer db.Close()
 
