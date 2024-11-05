f79d28bfb4191f435984a95cee46e36b0bcbeae7
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-05 13:56:02 +0700

    fix registration and add prev next button

--- src/frontend/src/routes/activity/[activity]/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ fetch, params }) => {
-  const response = await fetch(`/api/activities/${params}`);
-//   const activity = await response.json();
-  const activity = { id: '1', title: 'Activity 1', startDate: '2025-11-01', format: 'Project', description: 'This is description' };
-  return { activity };
-};
