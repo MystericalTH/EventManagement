431089d224fe97dbede8bc6364fe5db3a3afe35c
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-06 16:21:20 +0700

    feat fetch ActivityData

--- src/frontend/src/routes/activity/+page.ts
+++ src/frontend/src/routes/activity/+page.ts
@@ -2,11 +2,9 @@ import type { PageLoad } from './$types';
   
 export const load: PageLoad = async ({ fetch }) => {
   const response = await fetch('/api/activities');
-//   const activities = await response.json();
-  const activities = [
-    { id: 1, title: 'Activity 1', startDate: '2025-11-01', endDate: '2025-11-05', format: 'Project' },
-    { id: 2, title: 'Activity 2', startDate: '2024-11-01', endDate: '2024-11-30', format: 'Project' },
-    { id: 3, title: 'Activity 3', startDate: '2023-09-01', endDate: '2023-09-05', format: 'Workshop' }
-    ];
+  if (!response.ok) {
+    throw new Error(`Failed to load activities: ${response.status} ${response.statusText}`);
+  }
+  const activities = await response.json();
   return { activities };
 };
