690930976927cb2fac8eeb84efbd58c2c02aa7be
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-10 16:49:14 +0700

    feat: add AdminSidebar.svelte

---
+++ src/frontend/src/lib/components/sidebar/AdminSidebar.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Sidebar from '../Sidebar.svelte';
+	const sidebarItems = [
+		{
+			text: 'Activity',
+			href: null,
+			subitems: [
+				{ text: 'Manage Activities', href: '/admin/activities', subitems: null },
+				{ text: 'Activity Requests', href: '/admin/activities/requests', subitems: null },
+				{ text: 'See Feedback', href: '/admin/activities/feedback', subitems: null }
+			]
+		},
+		{
+			text: 'Members',
+			href: null,
+			subitems: [
+				{ text: 'Manage Members', href: '/admin/members', subitems: null },
+				{ text: 'Member Requests', href: '/admin/members/requests', subitems: null }
+			]
+		}
+	];
+</script>
+
+<Sidebar items={sidebarItems} />
