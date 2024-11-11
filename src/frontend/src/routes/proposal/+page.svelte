fb7813ee9831469f61e35b5db2d53d85b28a6298
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-11 15:35:02 +0700

    fix layout proposal

--- src/frontend/src/routes/proposal/+page.svelte
+++ src/frontend/src/routes/proposal/+page.svelte
@@ -71,70 +71,77 @@
 </script>
 
 <h1 class="text-center font-bold text-4xl my-5">Activity Proposal</h1>
-<form on:submit={handleProposalSubmit} class="flex flex-col w-72 mx-auto">
-  <div class="mb-4">
-    <label for="title" class="mb-2 font-bold">Title:</label>
-    <input type="text" id="title" bind:value={title} required class="p-2 text-lg border border-gray-300 rounded" />
-  </div>
-  <div class="mb-4">
-    <label for="format" class="mb-2 font-bold">Format:</label>
-    <select id="format" bind:value={format} required class="p-2 text-lg border border-gray-300 rounded">
-      <option value="" disabled selected>Select format</option>
-      <option value="project">Project</option>
-      <option value="workshop">Workshop</option>
-    </select>
-  </div>
 
-  {#if format === 'project'}
+<form on:submit={handleProposalSubmit} class="flex flex-wrap mx-auto w-8/12">
+  <!-- Left column -->
+  <div class="w-full md:w-1/2 pr-2 mt-4">
     <div class="mb-4">
-      <label for="startDate" class="mb-2 font-bold">Start Date:</label>
-      <input type="text" id="startDate" bind:value={startDate} required class="p-2 text-lg border border-gray-300 rounded" />
+      <label for="title" class="mb-2 font-bold">Title:</label>
+      <input type="text" id="title" bind:value={title} required class="p-2 text-lg border border-gray-300 rounded w-56" />
     </div>
     <div class="mb-4">
-      <label for="endDate" class="mb-2 font-bold">End Date:</label>
-      <input type="text" id="endDate" bind:value={endDate} required class="p-2 text-lg border border-gray-300 rounded" />
+      <label for="format" class="mb-2 font-bold">Format:</label>
+      <select id="format" bind:value={format} required class="p-2 text-lg border border-gray-300 rounded w-56">
+        <option value="" disabled selected>Select format</option>
+        <option value="project">Project</option>
+        <option value="workshop">Workshop</option>
+      </select>
     </div>
+    {#if format === 'project'}
+      <div class="mb-4">
+        <label for="startDate" class="mb-2 font-bold">Start Date:</label>
+        <input type="text" id="startDate" bind:value={startDate} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+      <div class="mb-4">
+        <label for="endDate" class="mb-2 font-bold">End Date:</label>
+        <input type="text" id="endDate" bind:value={endDate} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+      <div class="mb-4">
+        <label for="advisor" class="mb-2 font-bold">Advisor:</label>
+        <input type="text" id="advisor" bind:value={advisor} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+    {:else if format === 'workshop'}
+      <div class="mb-4">
+        <label for="startDate" class="mb-2 font-bold">Start Date:</label>
+        <input type="text" id="startDate" bind:value={startDate} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+      <div class="mb-4">
+        <label for="startTime" class="mb-2 font-bold">Start Time:</label>
+        <input type="text" id="startTime" bind:value={startTime} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+      <div class="mb-4">
+        <label for="endDate" class="mb-2 font-bold">End Date:</label>
+        <input type="text" id="endDate" bind:value={endDate} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+      <div class="mb-4">
+        <label for="endTime" class="mb-2 font-bold">End Time:</label>
+        <input type="text" id="endTime" bind:value={endTime} required class="p-2 text-lg border border-gray-300 rounded w-56" />
+      </div>
+    {/if}
     <div class="mb-4">
-      <label for="advisor" class="mb-2 font-bold">Advisor:</label>
-      <input type="text" id="advisor" bind:value={advisor} required class="p-2 text-lg border border-gray-300 rounded" />
+      <label for="maxNumber" class="mb-2 font-bold">Number of participant:</label>
+      <input type="number" id="maxNumber" bind:value={maxNumber} required class="p-2 text-lg border border-gray-300 rounded w-28" />
     </div>
-  {:else if format === 'workshop'}
-    <div class="mb-4">
-      <label for="startDate" class="mb-2 font-bold">Start Date:</label>
-      <input type="text" id="startDate" bind:value={startDate} required class="p-2 text-lg border border-gray-300 rounded" />
-    </div>
-    <div class="mb-4">
-      <label for="startTime" class="mb-2 font-bold">Start Time:</label>
-      <input type="text" id="startTime" bind:value={startTime} required class="p-2 text-lg border border-gray-300 rounded" />
-    </div>
-    <div class="mb-4">
-      <label for="endDate" class="mb-2 font-bold">End Date:</label>
-      <input type="text" id="endDate" bind:value={endDate} required class="p-2 text-lg border border-gray-300 rounded" />
-    </div>
-    <div class="mb-4">
-      <label for="endTime" class="mb-2 font-bold">End Time:</label>
-      <input type="text" id="endTime" bind:value={endTime} required class="p-2 text-lg border border-gray-300 rounded" />
-    </div>
-  {/if}
-  
-  <div class="mb-4">
-    <label for="maxNumber" class="mb-2 font-bold">Number of participant:</label>
-    <input type="number" id="maxNumber" bind:value={maxNumber} required class="p-2 text-lg border border-gray-300 rounded" />
   </div>
-  <div class="mb-4">
-    <label for="description" class="mb-2 font-bold">Description:</label>
-    <textarea id="description" bind:value={description} required class="p-2 text-lg border border-gray-300 rounded"></textarea>
+  <!-- Right column -->
+  <div class="w-full md:w-1/2 pl-2">
+    <div class="mb-4">
+      <label for="description" class="mb-2 font-bold block">Description:</label>
+      <textarea id="description" bind:value={description} required class="p-2 text-lg border border-gray-300 rounded w-56"></textarea>
+    </div>
+    <div class="mb-4">
+      <label for="activityRole" class="mb-2 font-bold block">Activity Role:</label>
+      <input type="text" id="newActivityRole" bind:value={newActivityRole} class="p-2 text-lg border border-gray-300 rounded w-56" />
+      <button type="button" on:click={addActivityRole} class="p-2 text-lg bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-700 mt-2 block">Add Role</button>
+      <ul class="mt-2">
+        {#each activityRole as role}
+          <li class="list-disc ml-5">{role}</li>
+        {/each}
+      </ul>
+    </div>
   </div>
-  <div class="mb-4">
-    <label for="activityRole" class="mb-2 font-bold">Activity Role:</label>
-    <input type="text" id="newActivityRole" bind:value={newActivityRole} class="p-2 text-lg border border-gray-300 rounded" />
-    <button type="button" on:click={addActivityRole} class="p-2 text-lg bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-700 mt-2">Add Role</button>
-    <ul class="mt-2">
-      {#each activityRole as role}
-        <li class="list-disc ml-5">{role}</li>
-      {/each}
-    </ul>
+  <!-- Submit button centered -->
+  <div class="w-full mt-4 text-center">
+    <button type="submit" class="p-2 text-lg bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-700 w-72">Submit</button>
   </div>
-
-  <button type="submit" class="p-2 text-lg bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-700 mt-4">Submit</button>
 </form>
