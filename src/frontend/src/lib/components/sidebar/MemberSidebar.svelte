a1f58d57c17214831de667802d72ccadbf25428c
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-12 07:02:36 +0700

    fix: remove member layout for sidebar

--- src/frontend/src/lib/components/sidebar/MemberSidebar.svelte
+++ src/frontend/src/lib/components/sidebar/MemberSidebar.svelte
@@ -1,1 +1,18 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import BaseSidebar from './BaseSidebar.svelte';
+	const items = [
+		{
+			text: 'Activity',
+			subitems: [
+				{ text: 'Your Activities', href: '/member/activities' },
+				{ text: 'Your Proposals', href: '/member/activities/proposals' }
+			]
+		},
+		{
+			text: 'Propose New Activity',
+			href: '/member/activities/propose'
+		}
+	];
+</script>
+
+<BaseSidebar {items} />
