b580afff9c09a07ff5dbc10f2ea915c0948421bb
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-13 13:58:04 +0700

    build: fix MSW support by using hooks and third-party plugins

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -4,30 +4,13 @@
 	// Loaded from .env.local, guide covers this
 	// step in a moment.
 	let { data, children } = $props();
-	console.log(data);
-	const isMswEnabled = import.meta.env.VITE_MSW_ENABLED === 'true';
-	// Flag to defer rendering of components
-
-	// until certain criteria are met on dev,
-	// e.g. MSW init.
-	let isReady = $state(!isMswEnabled);
-	if (isMswEnabled) {
-		console.log(isMswEnabled);
-		import('$msw')
-			.then((res) => res.inject())
-			.then(() => {
-				isReady = true;
-			});
-	}
 </script>
 
 <main>
-	{#if isReady}
 		<div class="flex h-screen">
 			<Sidebar role={data.role} />
 			<div class="flex-1">
 				{@render children()}
 			</div>
 		</div>
-	{/if}
 </main>
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vitest/config';
 import { loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import * as path from 'path';
+import { handlers } from './src/mocks/handlers';
+import msw from '@iodigital/vite-plugin-msw';
 
 export default defineConfig(({ command, mode }) => {
 	// Load env file based on `mode` in the current working directory.
 	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
-		plugins: [sveltekit()],
+		plugins: [sveltekit(), msw({ handlers })],
 
 		test: {
 			include: ['src/**/*.{test,spec}.{js,ts}'],
