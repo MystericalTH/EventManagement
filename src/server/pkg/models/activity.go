28c68f97f1209f3a2356e94c6b6a7a93dd6f1b78
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-15 19:45:36 +0700

    fix type

--- src/server/pkg/models/activity.go
+++ src/server/pkg/models/activity.go
@@ -1,18 +1,21 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Activity struct {
-	ID                int       `json:"id"`
-	Title             string    `json:"title"`
-	Proposer          int       `json:"proposer"`
-	StartDate         time.Time `json:"startDate"`
-	EndDate           time.Time `json:"endDate"`
-	MaxNumber         int       `json:"maxNumber"`
-	Format            string    `json:"format"`
-	Description       string    `json:"description"`
-	ProposeDateTime   time.Time `json:"proposeDateTime"`
-	AcceptAdmin       int       `json:"acceptAdmin"`
-	AcceptDateTime    time.Time `json:"acceptDateTime"`
-	ApplicationStatus string    `json:"applicationStatus"`
+	ID                int32          `json:"id"`
+	Title             string         `json:"title"`
+	Proposer          int32          `json:"proposer"`
+	StartDate         time.Time      `json:"startDate"`
+	EndDate           time.Time      `json:"endDate"`
+	MaxNumber         sql.NullInt32  `json:"maxNumber"`
+	Format            sql.NullString `json:"format"`
+	Description       string         `json:"description"`
+	ProposeDateTime   time.Time      `json:"proposeDateTime"`
+	AcceptAdmin       int32          `json:"acceptAdmin"`
+	AcceptDateTime    time.Time      `json:"acceptDateTime"`
+	ApplicationStatus string         `json:"applicationStatus"`
 }
