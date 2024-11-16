2adba5b6def6998a1aa16096be08c2894c1a479d
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-16 12:13:56 +0700

    feat GetFeedbackStatus

--- src/server/go.mod
+++ src/server/go.mod
@@ -5,6 +5,7 @@ go 1.23.2
 require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/sessions v1.4.0
 	golang.org/x/oauth2 v0.24.0
 )
--- src/server/pkg/api/routes.go
+++ src/server/pkg/api/routes.go
@@ -3,7 +3,16 @@ package api
 import (
 	"net/http"
 	api "sinno-server/pkg/api/handler"
+
+	"github.com/gorilla/mux"
 )
+
+func LogRoutes() {
+	http.HandleFunc("/api/login", HandleLogin)
+	http.HandleFunc("/api/auth/google/callback", HandleCallback)
+	http.HandleFunc("/api/logout", HandleLogout)
+	http.HandleFunc("/api/verify", api.HandleVerifyRole)
+}
 
 func ActivitiesRoutes() {
 	http.HandleFunc("/api/activities", api.GetActivities)
@@ -11,9 +20,6 @@ func ActivitiesRoutes() {
 	http.HandleFunc("/api/proposal/submit", api.PostActivity)
 }
 
-func LogRoutes() {
-	http.HandleFunc("/api/login", HandleLogin)
-	http.HandleFunc("/api/auth/google/callback", HandleCallback)
-	http.HandleFunc("/api/logout", HandleLogout)
-	http.HandleFunc("/api/verify", api.HandleVerifyRole)
+func FeedbackRoutes(router *mux.Router) {
+	router.HandleFunc("/api/activities/{activityId}/feedback/status", api.GetFeedbackStatus).Methods("GET")
 }
