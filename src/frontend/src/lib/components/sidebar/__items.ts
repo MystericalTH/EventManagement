ca02cc70c79292323610d3d75b3a55f9d7fcc78e
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-16 19:18:12 +0700

    style: npm run format on __items.ts

--- src/frontend/src/lib/components/sidebar/__items.ts
+++ src/frontend/src/lib/components/sidebar/__items.ts
@@ -1,48 +1,47 @@
-import type { SidebarItem } from "$lib/types/sidebar";
+import type { SidebarItem } from '$lib/types/sidebar';
 
 export const defaultItems: SidebarItem[] = [
-  {
-    text: 'Signup',
-    href: '/api/signup'
-  },
-  {
-    text: 'Login',
-    subitems: [
-      { text: 'Member', href: '/api/login?role=member' },
-      { text: 'Admin', href: '/api/login?role=admin' }
-    ]
-  }
-]
+	{
+		text: 'Signup',
+		href: '/api/signup'
+	},
+	{
+		text: 'Login',
+		subitems: [
+			{ text: 'Member', href: '/api/login?role=member' },
+			{ text: 'Admin', href: '/api/login?role=admin' }
+		]
+	}
+];
 
 export const adminItems: SidebarItem[] = [
-  {
-    text: 'Activity',
-    subitems: [
-      { text: 'Manage Activities', href: '/admin/activities' },
-      { text: 'Activity Requests', href: '/admin/activities/requests' },
-      { text: 'See Feedback', href: '/admin/activities/feedback' }
-    ]
-  },
-  {
-    text: 'Members',
-    subitems: [
-      { text: 'Manage Members', href: '/admin/members' },
-      { text: 'Member Requests', href: '/admin/members/requests' }
-    ]
-  }
+	{
+		text: 'Activity',
+		subitems: [
+			{ text: 'Manage Activities', href: '/admin/activities' },
+			{ text: 'Activity Requests', href: '/admin/activities/requests' },
+			{ text: 'See Feedback', href: '/admin/activities/feedback' }
+		]
+	},
+	{
+		text: 'Members',
+		subitems: [
+			{ text: 'Manage Members', href: '/admin/members' },
+			{ text: 'Member Requests', href: '/admin/members/requests' }
+		]
+	}
 ];
 
 export const memberItems: SidebarItem[] = [
-  {
-    text: 'Activity',
-    subitems: [
-      { text: 'Your Activities', href: '/member/activities' },
-      { text: 'Your Proposals', href: '/member/activities/proposals' }
-    ]
-  },
-  {
-    text: 'Propose New Activity',
-    href: '/member/activities/propose'
-  }
+	{
+		text: 'Activity',
+		subitems: [
+			{ text: 'Your Activities', href: '/member/activities' },
+			{ text: 'Your Proposals', href: '/member/activities/proposals' }
+		]
+	},
+	{
+		text: 'Propose New Activity',
+		href: '/member/activities/propose'
+	}
 ];
-
