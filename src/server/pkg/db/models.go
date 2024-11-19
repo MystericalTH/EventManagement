5bba3ca5ad8f2417d310f37f92bd29d654ab8d16
Natchapol Lebkrut <natchaponlt@gmail.com>
2024-11-19 14:58:13 +0700

    edit scheme dependencies

--- src/server/pkg/db/models.go
+++ src/server/pkg/db/models.go
@@ -6,22 +6,75 @@ package db
 
 import (
 	"database/sql"
+	"database/sql/driver"
+	"fmt"
 	"time"
 )
 
+type ActivityActivitytype string
+
+const (
+	ActivityActivitytypeProject  ActivityActivitytype = "Project"
+	ActivityActivitytypeWorkshop ActivityActivitytype = "Workshop"
+)
+
+func (e *ActivityActivitytype) Scan(src interface{}) error {
+	switch s := src.(type) {
+	case []byte:
+		*e = ActivityActivitytype(s)
+	case string:
+		*e = ActivityActivitytype(s)
+	default:
+		return fmt.Errorf("unsupported scan type for ActivityActivitytype: %T", src)
+	}
+	return nil
+}
+
+type NullActivityActivitytype struct {
+	ActivityActivitytype ActivityActivitytype `json:"activity_activitytype"`
+	Valid                bool                 `json:"valid"` // Valid is true if ActivityActivitytype is not NULL
+}
+
+// Scan implements the Scanner interface.
+func (ns *NullActivityActivitytype) Scan(value interface{}) error {
+	if value == nil {
+		ns.ActivityActivitytype, ns.Valid = "", false
+		return nil
+	}
+	ns.Valid = true
+	return ns.ActivityActivitytype.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (ns NullActivityActivitytype) Value() (driver.Value, error) {
+	if !ns.Valid {
+		return nil, nil
+	}
+	return string(ns.ActivityActivitytype), nil
+}
+
 type Activity struct {
-	Activityid        int32          `json:"activityid"`
-	Title             string         `json:"title"`
-	Proposer          int32          `json:"proposer"`
-	Startdate         time.Time      `json:"startdate"`
-	Enddate           time.Time      `json:"enddate"`
-	Maxnumber         sql.NullInt32  `json:"maxnumber"`
-	Format            sql.NullString `json:"format"`
-	Description       string         `json:"description"`
-	Proposedatetime   time.Time      `json:"proposedatetime"`
-	Acceptadmin       sql.NullInt32  `json:"acceptadmin"`
-	Acceptdatetime    sql.NullTime   `json:"acceptdatetime"`
-	Applicationstatus sql.NullString `json:"applicationstatus"`
+	Activityid        int32                `json:"activityid"`
+	Title             string               `json:"title"`
+	Proposer          int32                `json:"proposer"`
+	Startdate         time.Time            `json:"startdate"`
+	Enddate           time.Time            `json:"enddate"`
+	Maxnumber         sql.NullInt32        `json:"maxnumber"`
+	Format            sql.NullString       `json:"format"`
+	Description       string               `json:"description"`
+	Proposedatetime   time.Time            `json:"proposedatetime"`
+	Acceptadmin       sql.NullInt32        `json:"acceptadmin"`
+	Acceptdatetime    sql.NullTime         `json:"acceptdatetime"`
+	Applicationstatus sql.NullString       `json:"applicationstatus"`
+	Activitytype      ActivityActivitytype `json:"activitytype"`
+}
+
+type Activityregistration struct {
+	Activityid  int32     `json:"activityid"`
+	Memberid    int32     `json:"memberid"`
+	Role        string    `json:"role"`
+	Expectation string    `json:"expectation"`
+	Datetime    time.Time `json:"datetime"`
 }
 
 type Activityrole struct {
@@ -68,14 +121,6 @@ type Member struct {
 	Acceptadmin    sql.NullInt32 `json:"acceptadmin"`
 }
 
-type Pjregist struct {
-	Projectid   int32     `json:"projectid"`
-	Memberid    int32     `json:"memberid"`
-	Role        string    `json:"role"`
-	Expectation string    `json:"expectation"`
-	Datetime    time.Time `json:"datetime"`
-}
-
 type Project struct {
 	Projectid int32          `json:"projectid"`
 	Advisor   sql.NullString `json:"advisor"`
@@ -85,12 +130,4 @@ type Workshop struct {
 	Workshopid int32     `json:"workshopid"`
 	Starttime  time.Time `json:"starttime"`
 	Endtime    time.Time `json:"endtime"`
-}
-
-type Wsregist struct {
-	Workshopid  int32     `json:"workshopid"`
-	Memberid    int32     `json:"memberid"`
-	Role        string    `json:"role"`
-	Expectation string    `json:"expectation"`
-	Datetime    time.Time `json:"datetime"`
 }
