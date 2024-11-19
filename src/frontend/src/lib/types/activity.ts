fef3829c265b37e1583f1e9ba6f60c28e9791637
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-19 18:03:23 +0700

    fix: type of ActivityData

--- src/frontend/src/lib/types/activity.ts
+++ src/frontend/src/lib/types/activity.ts
@@ -1,14 +1,14 @@
 export interface ActivityData {
 	id: number;
 	title: string;
-	proposer: number;
+	proposer: string;
 	startDate: string;
 	endDate: string;
 	maxNumber: number;
 	format: string;
 	description: string;
 	proposeDateTime: string;
-	acceptAdmin: number;
-	acceptDateTime: string;
-	applicationStatus: string;
+	acceptAdmin?: string;
+	acceptDateTime?: string;
+	applicationStatus?: string;
 }
