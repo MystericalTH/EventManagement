4e7caa4cf16f9a30632e57622cadbec2c7e68791
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-19 17:02:25 +0700

    fix: handle null response
    in member/requests routes

--- src/frontend/src/routes/admin/members/requests/+page.ts
+++ src/frontend/src/routes/admin/members/requests/+page.ts
@@ -3,6 +3,7 @@ import { adminState } from '$lib/states/adminStates.svelte';
 export const load: PageLoad = async ({ fetch }) => {
 	const res = await fetch(`/api/members/requests`);
 	let memberRequestList = await res.json();
+	if (memberRequestList == null) memberRequestList = [];
 	adminState.memberRequestList = memberRequestList;
 
 	return { memberRequestList };
