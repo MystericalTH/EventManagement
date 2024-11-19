05ddaea36cdc33b2e4fc65683d2e4da6404d6b8a
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-19 08:26:09 +0700

    fix: pagination logic
    - removeCachedrequest -> removeItem
    - input of pagination is unnecessarily a state

--- src/frontend/src/lib/utils/adminActions.ts
+++ src/frontend/src/lib/utils/adminActions.ts
@@ -1,8 +1,7 @@
 import type { MemberRequest } from '$lib/types/memberRequest';
-import { adminState } from '$lib/states/adminStates.svelte';
-const removeCachedRequest = (id: number, pagination: any) => {
-	adminState.memberRequestList.splice(
-		adminState.memberRequestList.findIndex((e: MemberRequest) => e.id === id),
+const removeItem = (id: number, pagination: any) => {
+	pagination.data.splice(
+		pagination.data.findIndex((e: MemberRequest) => e.id === id),
 		1
 	);
 	if (pagination.maxPage < pagination.pageBuffer) {
@@ -13,19 +12,31 @@ const removeCachedRequest = (id: number, pagination: any) => {
 export const rejectRequest = (id: number, pagination) => {
 	fetch(`/api/members/requests/${id}`, { method: 'DELETE' }).then((r) => {
 		if (r.status == 204) {
-			removeCachedRequest(id, pagination);
+			removeItem(id, pagination);
 		} else {
 			console.log('Not deleted');
 			// TODO:  add handling
 		}
 	});
 };
+
 export const approveRequest = (id: number, pagination) => {
 	fetch(`/api/members/requests/${id}/approve`, { method: 'POST' }).then((r) => {
 		if (r.status == 204) {
-			removeCachedRequest(id, pagination);
+			removeItem(id, pagination);
 		} else {
 			console.log('Not approved');
+			// TODO:  add handling
+		}
+	});
+};
+
+export const removeMember = (id: number, pagination) => {
+	fetch(`/api/members/requests/${id}`, { method: 'DELETE' }).then((r) => {
+		if (r.status == 204) {
+			removeItem(id, pagination);
+		} else {
+			console.log('Not deleted');
 			// TODO:  add handling
 		}
 	});
