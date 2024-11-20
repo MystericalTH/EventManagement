25ce0c7343ff0669d26e5c8381d5a1cb868b9951
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-20 11:52:19 +0700

    fix signup front

--- src/frontend/src/routes/signup/+page.svelte
+++ src/frontend/src/routes/signup/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	let fname = '';
-	let lname = '';
+	let name = '';
 	let email = '';
 	let phone = '';
-	let githuburl = '';
+	let githubUrl = '';
 	let interest = '';
 	let reason = '';
 
@@ -11,17 +10,16 @@
 		event.preventDefault();
 
 		const formData = {
-			fname,
-			lname,
+			name,
 			email,
 			phone,
-			githuburl,
+			githubUrl,
 			interest,
 			reason
 		};
 
 		try {
-			const response = await fetch('/api/member/request', {
+			const response = await fetch('/api/member', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
@@ -40,79 +38,87 @@
 	};
 </script>
 
-<h1 class="my-5 text-center text-4xl font-bold">Sign Up</h1>
-<form on:submit={handleMemberSubmit} class="mx-auto flex w-72 flex-col">
-	<div class="mb-4">
-        <label for="fName" class="mb-2 font-bold">First Name:</label>
-        <input
-            type="text"
-            id="fName"
-            bind:value={fname}
-            required
-            class="rounded border border-gray-300 p-2 text-lg"
-        />
-    </div>
-    <div class="mb-4">
-        <label for="lName" class="mb-2 font-bold">Last Name:</label>
-        <input
-            type="text"
-            id="lName"
-            bind:value={lname}
-            required
-            class="rounded border border-gray-300 p-2 text-lg"
-        />
-    </div>
-	<div class="mb-4">
-		<label for="email" class="mb-2 font-bold">Email:</label>
-		<input
-			type="email"
-			id="email"
-			bind:value={email}
-			required
-			class="rounded border border-gray-300 p-2 text-lg"
-		/>
-	</div>
-	<div class="mb-4">
-		<label for="phone" class="mb-2 font-bold">Phone:</label>
-		<input
-			type="tel"
-			id="phone"
-			bind:value={phone}
-			required
-			class="rounded border border-gray-300 p-2 text-lg"
-		/>
-	</div>
-	<div class="mb-4">
-		<label for="githubUrl" class="mb-2 font-bold">GitHub:</label>
-		<input
-			type="url"
-			id="githubUrl"
-			bind:value={githuburl}
-			required
-			class="rounded border border-gray-300 p-2 text-lg"
-		/>
-	</div>
-	<div class="mb-4">
-		<label for="interest" class="mb-2 font-bold">What is your interest:</label>
-		<textarea
-			id="interest"
-			bind:value={interest}
-			required
-			class="rounded border border-gray-300 p-2 text-lg"
-		></textarea>
-	</div>
-	<div class="mb-4">
-		<label for="reason" class="mb-2 font-bold">Why do you want to join the club:</label>
-		<textarea
-			id="reason"
-			bind:value={reason}
-			required
-			class="rounded border border-gray-300 p-2 text-lg"
-		></textarea>
-	</div>
-	<button
-		type="submit"
-		class="mt-4 cursor-pointer rounded bg-blue-500 p-2 text-lg text-white hover:bg-blue-700"
-		>Submit</button
-	>
-</form>
+<div class="min-w-[700px] border-2 border-gray-100 p-5 shadow-md">
+	<h1 class="my-5 text-center text-4xl font-bold">Signup as Our Member</h1>
+	<form onsubmit={handleMemberSubmit} class="flex flex-col py-4">
+		<div class="grid grid-cols-[40%_60%] gap-4 pr-2">
+			<div class="content-center text-right">
+				<label for="name" class="mb-2 font-bold">Name</label>
+			</div>
+			<div>
+				<input
+					type="text"
+					id="name"
+					bind:value={name}
+					required
+					class="w-72 rounded border border-gray-300 p-2 text-base"
+				/>
+			</div>
+			<div class="content-center text-right">
+				<label for="email" class="mb-2 font-bold">Email</label>
+			</div>
+			<div>
+				<input
+					type="email"
+					id="email"
+					bind:value={email}
+					required
+					class="w-72 rounded border border-gray-300 p-2 text-base"
+				/>
+			</div>
+			<div class="content-center text-right">
+				<label for="phone" class="mb-2 font-bold">Phone Number</label>
+			</div>
+			<div>
+				<input
+					type="tel"
+					id="phone"
+					bind:value={phone}
+					required
+					class="rounded border border-gray-300 p-2 text-base"
+				/>
+			</div>
+			<div class="content-center text-right">
+				<label for="githubUrl" class="mb-2 font-bold">GitHub</label>
+			</div>
+			<div>
+				<input
+					type="url"
+					id="githubUrl"
+					bind:value={githubUrl}
+					required
+					class="w-72 rounded border border-gray-300 p-2 text-base"
+				/>
+			</div>
+			<div class="mt-4 content-start text-right">
+				<label for="interest" class="mb-2 font-bold">What is your interest</label>
+			</div>
+			<div>
+				<textarea
+					id="interest"
+					bind:value={interest}
+					required
+					class="min-w-72 max-w-fit resize rounded border border-gray-300 p-2 text-base"
+				></textarea>
+			</div>
+			<div class="mt-4 content-start text-right">
+				<label for="reason" class="mb-2 font-bold">Why do you want to join the club</label>
+			</div>
+			<div>
+				<textarea
+					id="reason"
+					bind:value={reason}
+					required
+					class="min-w-72 max-w-fit resize rounded border border-gray-300 p-2 text-base"
+				></textarea>
+			</div>
+		</div>
+		<div class="flex justify-center">
+			<button
+				type="submit"
+				class="mt-4 w-40 cursor-pointer rounded bg-blue-500 p-2 text-base text-white hover:bg-blue-700"
+				>Submit</button
+			>
+		</div>
+	</form>
+</div>
