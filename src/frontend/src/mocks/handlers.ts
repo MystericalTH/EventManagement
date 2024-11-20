edf754de4322c4c0770902198d08f68b8a8ff177
Natchapol Lebkrut <natchaponlt@gmail.com>
2024-11-20 11:32:55 +0700

    fix mock api

--- src/frontend/src/mocks/handlers.ts
+++ src/frontend/src/mocks/handlers.ts
@@ -2,30 +2,30 @@ import { http } from 'msw';
 import { HttpResponse } from 'msw';
 import { memberData } from './__data';
 export const handlers = [
-	// Mock a GET request to "/api/users"
-	http.get('/api/users', ({ params }) => {
-		return HttpResponse.json([
-			{
-				name: 'Nattawin Yamprasert',
-				email: 'test@email.com'
-			},
-			{
-				name: 'EIEIE YAYY',
-				email: 'test2@email.com'
-			}
-		]);
-	}),
+	// // Mock a GET request to "/api/users"
+	// http.get('/api/users', ({ params }) => {
+	// 	return HttpResponse.json([
+	// 		{
+	// 			name: 'Nattawin Yamprasert',
+	// 			email: 'test@email.com'
+	// 		},
+	// 		{
+	// 			name: 'EIEIE YAYY',
+	// 			email: 'test2@email.com'
+	// 		}
+	// 	]);
+	// }),
 
-	http.get('/api/members/requests', ({ params }) => {
-		return HttpResponse.json(memberData);
-	}),
-	http.get('/api/members', ({}) => {
-		return HttpResponse.json(memberData);
-	}),
-	http.delete('/api/members/requests/:id', ({ params }) => {
-		return new HttpResponse(null, { status: 204 });
-	}),
-	http.post('/api/members/requests/:id/approve', ({ params }) => {
-		return new HttpResponse(null, { status: 204 });
-	})
+	// http.get('/api/members/requests', ({ params }) => {
+	// 	return HttpResponse.json(memberData);
+	// }),
+	// http.get('/api/members', ({}) => {
+	// 	return HttpResponse.json(memberData);
+	// }),
+	// http.delete('/api/members/requests/:id', ({ params }) => {
+	// 	return new HttpResponse(null, { status: 204 });
+	// }),
+	// http.post('/api/members/requests/:id/approve', ({ params }) => {
+	// 	return new HttpResponse(null, { status: 204 });
+	// })
 ];
