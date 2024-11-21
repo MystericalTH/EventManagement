86fe76dba012b2456bcf4a53edc2416047cc39b5
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-21 16:57:37 +0700

    feat: add ActivitySlider and modify Card

---
+++ src/frontend/src/lib/components/activity/ActivitySlider.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import moment from 'moment';
+	import Card from './Card.svelte';
+	import type { ActivityData } from '$lib/types/activity';
+	export let activities: ActivityData[];
+</script>
+
+<div class="mb-4 flex h-56 flex-row items-center space-x-16 overflow-x-scroll bg-gray-100 px-4">
+	{#if activities.length != 0}
+		{#each activities as activity}
+			<Card
+				title={activity.title}
+				startDate={moment(activity.startDate).format('D/M/YYYY')}
+				endDate={moment(activity.endDate).format('D/M/YYYY')}
+				format={activity.format}
+				href={`activities/${activity.id.toString()}`}
+			/>
+		{/each}
+	{:else}
+		<div class="flex-1 text-center">
+			<p class="text-gray-400">Nothing to see here</p>
+		</div>
+	{/if}
+</div>
--- src/frontend/src/lib/components/activity/Card.svelte
+++ src/frontend/src/lib/components/activity/Card.svelte
@@ -9,10 +9,12 @@
 
 <a
 	{href}
-	class="block h-48 w-72 flex-none rounded-lg border border-gray-300 p-4 shadow-md hover:bg-gray-100"
+	class="space-between bg-whiteshadow-md flex h-48 w-72 flex-none flex-col rounded-lg border border-gray-300 bg-white hover:border-indigo-500 hover:bg-indigo-500 hover:text-white"
 >
-	<div class="mb-4 text-lg font-bold">{title}</div>
-	<div class="flex w-full items-center justify-between text-sm text-gray-600">
+	<div class="mx-4 flex-1 border-b border-gray-400 py-4 text-lg font-bold">{title}</div>
+	<div
+		class="flex w-full items-center justify-between rounded-b-lg bg-white p-4 text-sm text-gray-600"
+	>
 		<span>
 			<span>{startDate}</span>
 			{#if endDate != startDate}
