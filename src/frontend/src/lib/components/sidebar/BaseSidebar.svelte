6240153be610b03f2d166e06c0d88292b5921abd
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-21 17:04:01 +0700

    fix: sidebar href is clickable at top level

--- src/frontend/src/lib/components/sidebar/BaseSidebar.svelte
+++ src/frontend/src/lib/components/sidebar/BaseSidebar.svelte
@@ -20,31 +20,40 @@
 	class="flex h-[100] w-48 flex-none flex-col justify-between bg-gray-700 p-4 font-custom text-base tracking-wider"
 >
 	<nav>
-		{#each items as item}
-			<div
-				role="button"
-				class="collapsible mb-2 flex cursor-pointer items-center justify-between text-white hover:text-indigo-300 hover:drop-shadow-lg"
-				on:click={() => toggleItem(item.text)}
-			>
-				<span>{item.text}</span>
-
-				{#if item.subitems}
-					<span class="arrow ml-2 text-xs">{$openItems.has(item.text) ? '▲' : '▼'}</span>
-				{/if}
-			</div>
-			{#if item.subitems && $openItems.has(item.text)}
-				<ul class="subitems pl-2 text-sm">
-					{#each item.subitems as subitem}
-						<li class="my-2">
+		<ul class="list-none">
+			{#each items as item}
+				<li>
+					<div
+						class="collapsible space-between mb-2 flex cursor-pointer items-center justify-between text-white hover:text-indigo-300 hover:drop-shadow-lg"
+						on:click={() => toggleItem(item.text)}
+					>
+						{#if item.subitems}
+							<button>
+								<span>{item.text}</span>
+								<span class="arrow ml-2 text-xs">{$openItems.has(item.text) ? '▲' : '▼'}</span>
+							</button>
+						{:else}
 							<a
 								class="text-white no-underline hover:text-indigo-300 hover:drop-shadow-lg"
-								href={subitem.href}>{subitem.text}</a
+								href={item.href}>{item.text}</a
 							>
-						</li>
-					{/each}
-				</ul>
-			{/if}
-		{/each}
+						{/if}
+					</div>
+				</li>
+				{#if item.subitems && $openItems.has(item.text)}
+					<ul class="subitems pl-2 text-sm">
+						{#each item.subitems as subitem}
+							<li class="my-2">
+								<a
+									class="text-white no-underline hover:text-indigo-300 hover:drop-shadow-lg"
+									href={subitem.href}>{subitem.text}</a
+								>
+							</li>
+						{/each}
+					</ul>
+				{/if}
+			{/each}
+		</ul>
 	</nav>
 	<div>
 		{#if hasLogout}
