a9e15ed6ae02bbf9bf8f402919aebb50dbf4b7f9
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-22 20:47:38 +0700

    feat: click on Card shows overlay instead

--- src/frontend/src/lib/components/activity/ActivitySlider.svelte
+++ src/frontend/src/lib/components/activity/ActivitySlider.svelte
@@ -2,12 +2,13 @@
 	import Card from './Card.svelte';
 	import type { Activity } from '$lib/types/activity';
 	export let activities: Activity[];
+	export let cardOnclick: (data: Activity) => void;
 </script>
 
 <div class="mb-4 flex h-56 flex-row items-center space-x-16 overflow-x-scroll bg-gray-100 px-4">
 	{#if activities.length != 0}
 		{#each activities as data}
-			<Card {data} href={`activities/${data.id.toString()}`} />
+			<Card {data} onclick={cardOnclick} />
 		{/each}
 	{:else}
 		<div class="flex-1 text-center">
--- src/frontend/src/lib/components/activity/Card.svelte
+++ src/frontend/src/lib/components/activity/Card.svelte
@@ -4,18 +4,22 @@
 	import { formatActivityDateTime } from '$lib/utils/activity';
 
 	export let data: Activity;
-	export let href;
+	export let onclick: (data: Activity) => void;
 </script>
 
-<a
-	{href}
+<button
+	onclick={() => onclick(data)}
 	class="space-between bg-whiteshadow-md flex h-48 w-72 flex-none flex-col rounded-lg border border-gray-300 bg-white hover:border-indigo-500 hover:bg-indigo-500 hover:text-white hover:shadow-lg"
 >
-	<div class="mx-4 flex-1 border-b border-gray-400 py-4 text-lg font-bold">{data.title}</div>
+	<div class="mx-4 w-64 flex-1 border-b border-gray-400 py-4 text-left text-lg font-bold">
+		{data.title}
+	</div>
 	<div
 		class="flex w-full items-center justify-between rounded-b-lg bg-white p-4 text-sm text-gray-600"
 	>
-		<span>{formatActivityDateTime(data)}</span>
+		<span class="border-r-200 ... w-44 truncate whitespace-nowrap text-left"
+			>{formatActivityDateTime(data)}</span
+		>
 		<span>{data.format}</span>
 	</div>
-</a>
+</button>
