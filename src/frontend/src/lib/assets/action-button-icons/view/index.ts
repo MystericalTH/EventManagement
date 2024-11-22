12ea61a640c7dc865c3e600ad2cf80292433b915
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-22 08:57:54 +0700

    feat: add action button icons
    - add View icon
    - add Cross icon

---
+++ src/frontend/src/lib/assets/action-button-icons/view/index.ts
@@ -0,0 +1,12 @@
+import Eye from './eye.svg';
+
+
+import type { ImageSource } from '$lib/types';
+
+let imgsrc: ImageSource = {
+  idle: Eye,
+  enter: Eye,
+  click: Eye
+};
+
+export default imgsrc;
