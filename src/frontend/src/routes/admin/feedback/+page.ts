c59ba323f780d32edcbafb4372e65be35b7829ed
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-23 07:10:30 +0700

    fix: Feedback page is functional

--- src/frontend/src/routes/admin/feedback/+page.ts
+++ src/frontend/src/routes/admin/feedback/+page.ts
@@ -2,7 +2,7 @@ import type { ActivityData } from '$lib/types/activity';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-  const response = await fetch('/api/activities');
+	const response = await fetch('/api/activities');
 	if (!response.ok) {
 		console.log('err');
 		throw new Error(`Failed to load activities: ${response.status} ${response.statusText}`);
