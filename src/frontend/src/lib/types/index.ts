7c2ba9e0d4657d8916057fdc8eaa8a6d9d3f35cc
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-23 07:08:24 +0700

    feat: add FeedbackData to types/index.ts

--- src/frontend/src/lib/types/index.ts
+++ src/frontend/src/lib/types/index.ts
@@ -4,5 +4,6 @@ import type { Member } from './member';
 import type { SidebarItem } from './sidebar';
 import type { Activity } from './activity';
 import type { ImageSource } from './actionButtonIcons';
+import type { FeedbackData } from './feedback';
 
-export type { Pagination, Member, Activity, SidebarItem, ImageSource };
+export type { Pagination, Member, Activity, SidebarItem, ImageSource, FeedbackData };
