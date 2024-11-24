2caab29be8b4c03b58e69a5a6140003826f068e0
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-24 15:53:46 +0700

    test: uncomment handlers.ts mocks

--- src/frontend/src/mocks/handlers.ts
+++ src/frontend/src/mocks/handlers.ts
@@ -2,19 +2,19 @@ import { http } from 'msw';
 import { HttpResponse } from 'msw';
 import { memberData, activityData, mockFeedbackData, memberRegistrationData } from './__data';
 export const handlers = [
-	// // Mock a GET request to "/api/users"
-	// http.get('/api/users', ({ params }) => {
-	// 	return HttpResponse.json([
-	// 		{
-	// 			name: 'Nattawin Yamprasert',
-	// 			email: 'test@email.com'
-	// 		},
-	// 		{
-	// 			name: 'EIEIE YAYY',
-	// 			email: 'test2@email.com'
-	// 		}
-	// 	]);
-	// }),
+	// Mock a GET request to "/api/users"
+	http.get('/api/users', ({ params }) => {
+		return HttpResponse.json([
+			{
+				name: 'Nattawin Yamprasert',
+				email: 'test@email.com'
+			},
+			{
+				name: 'EIEIE YAYY',
+				email: 'test2@email.com'
+			}
+		]);
+	}),
 
 	http.get('/api/members/requests', ({ params }) => {
 		return HttpResponse.json(memberData);
