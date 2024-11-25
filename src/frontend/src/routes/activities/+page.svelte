61ac1eea647004251baf40aa6b878474d091e199
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-11-25 18:46:07 +0700

    Change overlay to goto page
    No more overlay

--- src/frontend/src/routes/activities/+page.svelte
+++ src/frontend/src/routes/activities/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import type { Activity } from '$lib/types';
 	import ActivitySlider from '$lib/components/activity/ActivitySlider.svelte';
-	import { cross } from '$lib/assets/action-button-icons';
-	import Overlay from '$lib/components/Overlay.svelte';
-	import ActivityContent from '$lib/components/ActivityContent.svelte';
-	import ActionButton from '$lib/components/ActionButton.svelte';
+	import { goto } from '$app/navigation';
 
 	let { data }: { data: { activities: Array<Activity> } } = $props();
 
@@ -25,34 +22,21 @@
 		const endDate = new Date(activity.endDate);
 		return endDate < currentDate;
 	});
-	let showOverlay = $state(false);
+
 	let selectedActivity: Activity | null = $state(null);
 
-	function viewActivity(activity: Activity) {
+	function gotoActivity(activity: Activity) {
 		selectedActivity = activity;
-		showOverlay = true;
+		goto(`/activities/${activity.id}`);
 	}
-	const closeOverlay = () => {
-		selectedActivity = null;
-		showOverlay = false;
-	};
 </script>
 
 <div class="overflow-y-scroll">
 	<h1 class="mb-6 text-3xl font-bold">Activities</h1>
 	<h2 class="mb-2 text-xl">Upcoming</h2>
-	<ActivitySlider activities={upcomingActivities} cardOnclick={viewActivity} />
+	<ActivitySlider activities={upcomingActivities} cardOnclick={gotoActivity} />
 	<h2 class="mb-2 text-xl">Ongoing</h2>
-	<ActivitySlider activities={ongoingActivities} cardOnclick={viewActivity} />
+	<ActivitySlider activities={ongoingActivities} cardOnclick={gotoActivity} />
 	<h2 class="mb-2 text-xl">Completed</h2>
-	<ActivitySlider activities={completedActivities} cardOnclick={viewActivity} />
+	<ActivitySlider activities={completedActivities} cardOnclick={gotoActivity} />
 </div>
-
-<Overlay {showOverlay}>
-	<div class="flex justify-end">
-		<ActionButton imgsrc={cross} action={closeOverlay} width="20px" alt="Close" />
-	</div>
-	<div class="overflow-y-scroll">
-		<ActivityContent data={selectedActivity} />
-	</div>
-</Overlay>
