3685e946b64e44c6f208410a4a9bbbd00a9e6a88
Nattawin Yamprasert <nattawin.ya@gmail.com>
2024-11-25 20:00:11 +0700

    feat: add default error page

---
+++ src/frontend/src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+	import { error } from '@sveltejs/kit';
+</script>
+
+<div class="center">
+	<div>
+		<h1 class="text-6xl font-bold">{$page.status}</h1>
+		<p class="mt-4 text-3xl">{$page.error?.message}</p>
+	</div>
+</div>
+
+<style>
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		text-align: center;
+	}
+</style>
