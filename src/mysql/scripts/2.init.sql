c2483ea363633f0f0cd82f3c66c56662f1a74d44
Natchapol Lebkrut <natchaponlt@gmail.com>
2024-11-26 09:05:23 +0700

    revise sql code

--- src/mysql/scripts/2.init.sql
+++ src/mysql/scripts/2.init.sql
@@ -1,3 +1,5 @@
+USE clubmanagement;
+
 Insert into admin(fname,lname,email) values ('Natchapol','Lebkrut','6522781333@g.siit.tu.ac.th');
 
 Insert into development(fname,lname,email) values ('Nattawin','Yamprasert','6522781333@g.siit.tu.ac.th');
